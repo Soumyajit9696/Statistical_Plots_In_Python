15d97767b88639b015b951c5e49cd4fc04b779d0
Soumyajit Das <94384694+Soumyajit9696@users.noreply.github.com>
2022-11-19 20:23:45 +0530

    Add files via upload

---
+++ MB distribution.py.py
@@ -0,0 +1,28 @@
+"""
+Cu Roll:203012-21-0006
+Cu Reg:012-1111-0451-20
+"""
+#================================#
+# MAXWELL BOLLTZMAN DISTRIBUTION #
+#================================#
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+E=np.linspace(7.1,7.4,1000)
+mu=7.1
+k=0.0000865
+T=[100,200,300,400,500,600]
+
+for i in range(6):
+    f=lambda E:1/np.exp((E-mu)/(k*T[i]))
+    plt.plot(E,f(E),label='T='+str(T[i])+'k')
+    plt.legend()
+    
+plt.grid()
+plt.title(r'Maxwell-boltzmann Distribution [ $\frac{1}{\exp(\frac{E-\mu}{K_bT})}$]')
+plt.xlabel('E(Energy)$\longrightarrow$',size=10)
+plt.ylabel(r'$\frac{1}{\exp(\frac{E-\mu}{K_bT})}$$\longrightarrow$',size=18)
+plt.axhline(c='black')
+plt.show()
