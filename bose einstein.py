2482b146c844569913af06a7e619e084c599f918
Soumyajit Das <94384694+Soumyajit9696@users.noreply.github.com>
2022-11-19 20:21:59 +0530

    Add files via upload

---
+++ bose einstein.py
@@ -0,0 +1,29 @@
+"""
+Cu roll:203012-21-0006
+Cu Reg:012-1111-0451-20
+"""
+
+#============================#
+# Bose Einstein Distribution #
+#============================#
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+E=np.linspace(7.101,7.16,1000)
+mu=7.1
+k=0.0000865
+T=[100,200,300,400,500,600]
+
+for i in range(6):
+    f=lambda E:1/((np.exp((E-mu)/(k*T[i])))-1)
+    plt.plot(E,f(E),label='T='+str(T[i])+'k')
+    plt.legend()
+    
+plt.grid()
+plt.title(r'Bose-Einstein Distribution [ $\frac{1}{\exp(\frac{E-\mu}{K_bT})-1}$]')
+plt.xlabel('E(Energy)$\longrightarrow$',size=14)
+plt.ylabel(r'$\frac{1}{\exp(\frac{E-\mu}{K_bT})-1}\longrightarrow$',size=18)
+plt.axhline(c='black')
+plt.show()
+
